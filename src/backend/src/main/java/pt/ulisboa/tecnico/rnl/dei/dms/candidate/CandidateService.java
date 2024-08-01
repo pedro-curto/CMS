9e0b09ab3a0c3e194acfa47145b06d3f1d60b691
Pedro Curto <pedro.a.curto@tecnico.ulisboa.pt>
2024-08-01 16:16:01 +0100

    fix: added new candidate field update

--- src/backend/src/main/java/pt/ulisboa/tecnico/rnl/dei/dms/candidate/CandidateService.java
+++ src/backend/src/main/java/pt/ulisboa/tecnico/rnl/dei/dms/candidate/CandidateService.java
@@ -31,6 +31,7 @@ public class CandidateService {
     public CandidateDto updateCandidate(Long id, CandidateDto candidateDto) {
         Candidate candidate = candidateRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Candidate not found"));
+        candidate.setIstId(candidateDto.getIstId());
         candidate.setName(candidateDto.getName());
         candidate.setEmail(candidateDto.getEmail());
         candidateRepository.save(candidate);
