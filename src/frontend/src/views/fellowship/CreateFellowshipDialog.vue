a05037034c206434b8bf447217e764df5cbea33e
pedro.a.curto@tecnico.ulisboa.pt <pedroalexandrecurto@gmail.com>
2024-08-03 02:46:30 +0100

    feat: added input validation to fellowship creation and update

--- src/frontend/src/views/fellowship/CreateFellowshipDialog.vue
+++ src/frontend/src/views/fellowship/CreateFellowshipDialog.vue
@@ -16,37 +16,49 @@
       </v-card-title>
 
       <v-card-text>
-        <v-text-field
-            v-model="newFellowship.name"
-            label="Name"
-            required
-        ></v-text-field>
-
-        <div>
-          <label class="v-label">Start Date</label>
-          <VueDatePicker
-              v-model="newFellowship.startDate"
-              :enable-time-picker="false"
-              :auto-apply="true"
+        <v-form ref="form" v-model="valid" lazy-validation>
+          <v-text-field
+              v-model="newFellowship.name"
+              label="Name"
+              :rules="nameRules"
               required
-          />
-        </div>
+          ></v-text-field>
 
-        <div>
-          <label class="v-label">End Date</label>
-          <VueDatePicker
-              v-model="newFellowship.endDate"
-              :enable-time-picker="false"
-              :auto-apply="true"
+          <div>
+            <label class="v-label">Start Date</label>
+            <div class="date-picker-container">
+              <VueDatePicker
+                  v-model="newFellowship.startDate"
+                  :enable-time-picker="false"
+                  :auto-apply="true"
+                  :rules="dateRules"
+                  required
+              />
+              <span v-if="!newFellowship.startDate" class="error-message">Start Date is required</span>
+            </div>
+          </div>
+
+          <div>
+            <label class="v-label">End Date</label>
+            <div class="date-picker-container">
+              <VueDatePicker
+                  v-model="newFellowship.endDate"
+                  :enable-time-picker="false"
+                  :auto-apply="true"
+                  :rules="endDateRules"
+                  required
+              />
+              <span v-if="!newFellowship.endDate" class="error-message">End Date is required</span>
+            </div>
+          </div>
+
+          <v-text-field
+              v-model="newFellowship.monthlyValue"
+              label="Monthly Value"
+              :rules="monthlyValueRules"
               required
-          />
-        </div>
-
-        <v-text-field
-            v-model="newFellowship.monthlyValue"
-            label="Monthly Value"
-            required
-        ></v-text-field>
+          ></v-text-field>
+        </v-form>
       </v-card-text>
 
       <v-divider></v-divider>
@@ -57,13 +69,11 @@
         <v-btn text="Close" variant="plain" @click="dialog = false"></v-btn>
 
         <v-btn
-          color="primary"
-          text="Save"
-          variant="tonal"
-          @click="
-            dialog = false;
-            saveFellowship()
-       "
+            color="primary"
+            text="Save"
+            variant="tonal"
+            :disabled="!valid"
+            @click="submitForm"
         ></v-btn>
       </v-card-actions>
     </v-card>
@@ -71,13 +81,15 @@
 </template>
 
 <script setup lang="ts">
-import {ref, reactive, watch } from 'vue'
+import {ref, reactive, watch} from 'vue'
 import RemoteService from '@/services/RemoteService'
 import type FellowshipDto from '@/models/fellowship/FellowshipDto'
 import VueDatePicker from '@vuepic/vue-datepicker'
 import '@vuepic/vue-datepicker/dist/main.css'
 
 const dialog = ref(false)
+const valid = ref(false)
+const form = ref(null)
 const emit = defineEmits(['fellowship-created'])
 const newFellowship = reactive<FellowshipDto>({
   name: '',
@@ -85,6 +97,26 @@ const newFellowship = reactive<FellowshipDto>({
   endDate: '',
   monthlyValue: ''
 })
+
+// validation rules
+const nameRules = [
+  (v: string) => !!v || 'Name is required',
+  (v: string) => v.length > 2 || 'Fellowship name must have at least 3 characters'
+]
+
+const monthlyValueRules = [
+  (v: string) => !!v || 'Monthly Value is required',
+  (v: string) => v.length > 0 || 'Monthly Value must not be blank',
+  (v: string) => /^\d+(\.\d{1,2})?$/.test(v) || 'Monthly Value must be a valid monetary amount'
+]
+
+const dateRules = [
+  (v: string) => !!v || 'Date is required'
+]
+
+const endDateRules = [
+  (v: string) => !!v || 'End Date is required',
+]
 
 watch(dialog, (newVal) => {
   if (!newVal) {
@@ -99,9 +131,19 @@ const resetFellowship = () => {
   newFellowship.monthlyValue = ''
 }
 
-const saveFellowship = async () => {
-  await RemoteService.addFellowship(newFellowship)
-  dialog.value = false
-  emit('fellowship-created')
+const submitForm = async () => {
+  if (form.value && form.value.validate() && newFellowship.startDate && newFellowship.endDate) {
+    await RemoteService.addFellowship(newFellowship)
+    dialog.value = false
+    emit('fellowship-created')
+  }
 }
+
 </script>
+
+<style scoped>
+.error-message {
+  color: red;
+  font-size: 0.75rem;
+}
+</style>
--- src/frontend/src/views/fellowship/UpdateFellowshipDialog.vue
+++ src/frontend/src/views/fellowship/UpdateFellowshipDialog.vue
@@ -2,15 +2,23 @@
   <div class="pa-4 text-center">
       <v-card prepend-icon="mdi-briefcase-edit" title="Update Fellowship Details">
         <v-card-text>
-          <v-text-field label="Name*" required v-model="fellowship.name"></v-text-field>
+          <v-form ref="form" v-model="valid" lazy-validation>
+          <v-text-field
+              label="Name*"
+              required
+              :rules="nameRules"
+              v-model="fellowship.name"
+          ></v-text-field>
           <div>
             <label class="v-label">Start Date</label>
             <VueDatePicker
                 v-model="fellowship.startDate"
                 :enable-time-picker="false"
                 :auto-apply="true"
+                :rules="dateRules"
                 required
             />
+            <span v-if="!fellowship.startDate" class="error-message">Start Date is required</span>
           </div>
           <div>
             <label class="v-label">End Date</label>
@@ -18,10 +26,18 @@
                 v-model="fellowship.endDate"
                 :enable-time-picker="false"
                 :auto-apply="true"
+                :rules="endDateRules"
                 required
             />
+            <span v-if="!fellowship.endDate" class="error-message">End Date is required</span>
           </div>
-          <v-text-field label="Monthly Value*" required v-model="fellowship.monthlyValue"></v-text-field>
+          <v-text-field
+              label="Monthly Value*"
+              required
+              :rules="monthlyValueRules"
+              v-model="fellowship.monthlyValue"
+          ></v-text-field>
+          </v-form>
         </v-card-text>
 
         <v-divider></v-divider>
@@ -35,7 +51,8 @@
               color="primary"
               text="Save"
               variant="tonal"
-              @click="saveFellowship"
+              :disabled="!valid"
+              @click="submitForm"
           ></v-btn>
         </v-card-actions>
       </v-card>
@@ -48,6 +65,8 @@ import RemoteService from '@/services/RemoteService'
 import type FellowshipDto from '@/models/fellowship/FellowshipDto'
 import VueDatePicker from '@vuepic/vue-datepicker'
 
+const form = ref(null)
+const valid = ref(false)
 const props = defineProps<{ fellowship: FellowshipDto }>()
 const dialog = ref(false)
 const fellowship = ref<FellowshipDto>({
@@ -55,6 +74,26 @@ const fellowship = ref<FellowshipDto>({
 })
 
 const emit = defineEmits(['fellowship-updated', 'dialog-close'])
+
+// validation rules
+const nameRules = [
+  (v: string) => !!v || 'Name is required',
+  (v: string) => v.length > 2 || 'Fellowship name must have at least 3 characters'
+]
+
+const monthlyValueRules = [
+  (v: string) => !!v || 'Monthly Value is required',
+  (v: string) => v.length > 0 || 'Monthly Value must not be blank',
+  (v: string) => /^\d+(\.\d{1,2})?$/.test(v) || 'Monthly Value must be a valid monetary amount'
+]
+
+const dateRules = [
+  (v: string) => !!v || 'Date is required'
+]
+
+const endDateRules = [
+  (v: string) => !!v || 'End Date is required',
+]
 
 watch(props, (newProps)=> {
   fellowship.value = { ...newProps.fellowship }
@@ -64,10 +103,19 @@ const closeDialog = () => {
   emit('dialog-close')
 }
 
-const saveFellowship = async () => {
-  await RemoteService.updateFellowship(fellowship.value.id, fellowship.value)
-  emit('fellowship-updated')
-  closeDialog()
+const submitForm = async () => {
+  console.log(valid.value)
+  if (form.value && form.value.validate()) {
+    await RemoteService.updateFellowship(fellowship.value)
+    emit('fellowship-updated')
+    closeDialog()
+  }
 }
 
 </script>
+
+<style scoped>
+.error-message {
+  color: red;
+}
+</style>
