71f0b51a9a559e609a0782325a643ea95591d9c4
Pedro Curto <pedro.a.curto@tecnico.ulisboa.pt>
2024-08-05 15:18:08 +0100

    feat: added new classes to handle errors

---
+++ src/backend/src/main/java/pt/ulisboa/tecnico/rnl/dei/dms/error/ErrorMessage.java
@@ -0,0 +1,28 @@
+package pt.ulisboa.tecnico.rnl.dei.dms.error;
+
+public enum ErrorMessage {
+    // Evaluation
+   ENROLLMENT_NOT_FOUND("Enrollment with id %s not found"),
+
+    EVALUATION_NOT_FOUND("Evaluation for enrollment id %s not found"),
+
+    EVALUATION_ALREADY_EXISTS("Evaluation already exists"),
+
+    FELLOWSHIP_NOT_FOUND("Fellowship with id %s not found"),
+
+    NO_ENROLLMENTS_FOUND("No enrollments found"),
+
+    NO_WINNER_FOUND("No winner found");
+
+   // Fellowship
+
+    // Candidate
+
+    // Enrollment
+
+    public final String label;
+
+    ErrorMessage(String label) {
+        this.label = label;
+    }
+}
